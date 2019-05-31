f568981f479968205f58b7ec97af8cbfdb29d946
Muhammad Rifqi Prabantoro <45098478+mrifqip29@users.noreply.github.com>
2019-05-31 23:56:35 +0700

    Bismillah

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "rifqiazure.database.windows.net";
+    $user = "rifqionky";
+    $pass = "Blackblues4m";
+    $db = "rifqidb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
