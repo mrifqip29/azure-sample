5e42da9b01adf5789ef5957b86de7902c2565af4
Muhammad Rifqi Prabantoro <45098478+mrifqip29@users.noreply.github.com>
2019-06-03 22:22:36 +0700

    ganti buat submission 2

---
+++ azurecv.php
@@ -0,0 +1,135 @@
+<?php
+if (isset($_POST['submit'])) {
+	if (isset($_POST['url'])) {
+		$url = $_POST['url'];
+	} else {
+		header("Location: phpQS.php");
+	}
+} else {
+	header("Location: phpQS.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Analisis Sample Foto</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+			<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="https://azurerifqi.azurewebsites.net/">Home</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="https://azurerifqi.azurewebsites.net/phpQS.php">Analisis Foto<span class="sr-only">(current)</span></a>
+			</li>
+		</div>
+		</nav>
+		<main role="main" class="container">
+    		<div class="starter-template"> <br><br>
+        		<h1>Hasil Analisis Foto</h1>
+				<span class="border-top my-3"></span>
+            </div>
+            
+<script type="text/javascript">
+    function processImage() {
+       
+        var subscriptionKey = "ae1d4fab8ae2491ab0803f3bac58ebc7";
+ 
+        var uriBase =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+ 
+        // Request parameters.
+        var params = {
+            "visualFeatures": "Categories,Description,Color",
+            "details": "",
+            "language": "en",
+        };
+ 
+        // Display the image.
+
+        var sourceImageUrl = "<?php echo $url ?>";
+        document.querySelector("#sourceImage").src = sourceImageUrl;
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+            $("#description").text(data.description.captions[0].text);
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+            alert(errorString);
+        });
+    };
+</script>
+ 
+<!-- <h1>Analyze image:</h1>
+Enter the URL to an image, then click the <strong>Analyze image</strong> button.
+<br><br>
+Image to analyze:
+<input type="text" name="inputImage" id="inputImage"
+    value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
+<button onclick="processImage()">Analyze image</button>
+<br><br> -->
+<br>
+<div id="wrapper" style="width:1020px; display:table;">
+    <div id="jsonOutput" style="width:600px; display:table-cell;">
+        Response:
+        <br><br>
+        <textarea id="responseTextArea" class="UIInput"
+                  style="width:580px; height:400px;"></textarea>
+    </div>
+    <div id="imageDiv" style="width:420px; display:table-cell;">
+        Source image:
+        <br><br>
+        <img id="sourceImage" width="400" />
+        <br>
+		<h3 id="description">Loading description. . .</h3>
+    </div>
+</div>
+
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+  </body>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,89 +1,119 @@
 <html>
  <head>
- <Title>Registration Form</Title>
- <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
- 	}
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
- 	h1 { font-size: 2em; }
- 	h2 { font-size: 1.75em; }
- 	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
- </style>
+    <Title>Analisis Sample Foto</Title>
+    <!-- <style type="text/css">
+        body { background-color: #fff; border-top: solid 10px #000;
+            color: #333; font-size: .85em; margin: 20; padding: 20;
+            font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+        }
+        h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+        h1 { font-size: 2em; }
+        h2 { font-size: 1.75em; }
+        h3 { font-size: 1.2em; }
+        table { margin-top: 0.75em; }
+        th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+        td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+    </style> -->
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+
+    
  </head>
  <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br></br>
-       Email <input type="text" name="email" id="email"/></br></br>
-       Job <input type="text" name="job" id="job"/></br></br>
-       <input type="submit" name="submit" value="Submit" />
-       <input type="submit" name="load_data" value="Load Data" />
- </form>
- <?php
-    $host = "rifqiazure.database.windows.net";
-    $user = "rifqionky";
-    $pass = "Blackblues4m";
-    $db = "rifqidb";
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+			<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="https://azurerifqi.azurewebsites.net/index.php">Home</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="https://azurerifqi.azurewebsites.net/phpQS.php">Analisis Foto<span class="sr-only">(current)</span></a>
+			</li>
+		</div>
+		</nav>
+    <h1>Register here!</h1>
+    <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+    <form method="post" action="index.php" enctype="multipart/form-data" >
+        Name  <input type="text" name="name" id="name"/></br></br>
+        Email <input type="text" name="email" id="email"/></br></br>
+        Job <input type="text" name="job" id="job"/></br></br>
+        <input type="submit" name="submit" value="Submit" />
+        <input type="submit" name="load_data" value="Load Data" />
+    </form>
+    <?php
+        $host = "rifqiazure.database.windows.net";
+        $user = "rifqionky";
+        $pass = "Blackblues4m";
+        $db = "rifqidb";
 
-    try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
-    }
-
-    if (isset($_POST['submit'])) {
         try {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $job = $_POST['job'];
-            $date = date("Y-m-d");
-            // Insert data
-            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
-                        VALUES (?,?,?,?)";
-            $stmt = $conn->prepare($sql_insert);
-            $stmt->bindValue(1, $name);
-            $stmt->bindValue(2, $email);
-            $stmt->bindValue(3, $job);
-            $stmt->bindValue(4, $date);
-            $stmt->execute();
+            $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+            $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         } catch(Exception $e) {
             echo "Failed: " . $e;
         }
 
-        echo "<h3>Your're registered!</h3>";
-    } else if (isset($_POST['load_data'])) {
-        try {
-            $sql_select = "SELECT * FROM Registration";
-            $stmt = $conn->query($sql_select);
-            $registrants = $stmt->fetchAll(); 
-            if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
-                echo "<table>";
-                echo "<tr><th>Name</th>";
-                echo "<th>Email</th>";
-                echo "<th>Job</th>";
-                echo "<th>Date</th></tr>";
-                foreach($registrants as $registrant) {
-                    echo "<tr><td>".$registrant['name']."</td>";
-                    echo "<td>".$registrant['email']."</td>";
-                    echo "<td>".$registrant['job']."</td>";
-                    echo "<td>".$registrant['date']."</td></tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "<h3>No one is currently registered.</h3>";
+        if (isset($_POST['submit'])) {
+            try {
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $job = $_POST['job'];
+                $date = date("Y-m-d");
+                // Insert data
+                $sql_insert = "INSERT INTO Registration (name, email, job, date) 
+                            VALUES (?,?,?,?)";
+                $stmt = $conn->prepare($sql_insert);
+                $stmt->bindValue(1, $name);
+                $stmt->bindValue(2, $email);
+                $stmt->bindValue(3, $job);
+                $stmt->bindValue(4, $date);
+                $stmt->execute();
+            } catch(Exception $e) {
+                echo "Failed: " . $e;
             }
-        } catch(Exception $e) {
-            echo "Failed: " . $e;
+
+            echo "<h3>Your're registered!</h3>";
+        } else if (isset($_POST['load_data'])) {
+            try {
+                $sql_select = "SELECT * FROM Registration";
+                $stmt = $conn->query($sql_select);
+                $registrants = $stmt->fetchAll(); 
+                if(count($registrants) > 0) {
+                    echo "<h2>Total File : ".count($registrants)." Orang</h2>";
+                    echo "<h2>People who are registered:</h2>";
+                    echo "<table>";
+                    echo "<tr><th>Name</th>";
+                    echo "<th>Email</th>";
+                    echo "<th>Job</th>";
+                    echo "<th>Date</th></tr>";
+                    foreach($registrants as $registrant) {
+                        echo "<tr><td>".$registrant['name']."</td>";
+                        echo "<td>".$registrant['email']."</td>";
+                        echo "<td>".$registrant['job']."</td>";
+                        echo "<td>".$registrant['date']."</td></tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    echo "<h3>No one is currently registered.</h3>";
+                }
+            } catch(Exception $e) {
+                echo "Failed: " . $e;
+            }
         }
-    }
- ?>
+    ?>
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+  </body>
  </body>
  </html>
