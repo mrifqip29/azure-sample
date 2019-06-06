44b352d4e354d50a2c0a774c46a2a422489ebcee
Muhammad Rifqi Prabantoro <45098478+mrifqip29@users.noreply.github.com>
2019-06-06 10:22:51 +0700

    k

--- azurecv.php
+++ azurecv.php
@@ -48,12 +48,28 @@ if (isset($_POST['submit'])) {
             height: 100%; 
             
             /* Center and scale the image nicely */
-            background-position: center;
+            background-position: center center;
             background-repeat: no-repeat;
             background-size: cover;
+            background-attachment: fixed;
             }
 
             /* Position text in the middle of the page/image */
+            .bg-text {
+                background-color: rgb(0,0,0); /* Fallback color */
+                background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+                color: white;
+                font-weight: bold;
+                border: 3px solid #f1f1f1;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                z-index: 2;
+                width: 80%;
+                padding: 20px;
+                text-align: center;
+            }
             }
         </style> 
         
@@ -74,7 +90,7 @@ if (isset($_POST['submit'])) {
                 </li> -->
             </div>
 	</nav>
-	<main role="main" class="container">
+	<main role="main" class="container bg-text">
         <br>
     	<div class="starter-template"> <br><br>
         	<h1>Hasil Analisis Foto</h1>
--- phpQS.php
+++ phpQS.php
@@ -60,12 +60,27 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
             height: 100%; 
             
             /* Center and scale the image nicely */
-            background-position: center;
+            background-position: center center;
             background-repeat: no-repeat;
             background-size: cover;
+            background-attachment: fixed;
             }
 
             /* Position text in the middle of the page/image */
+            .bg-text {
+                background-color: rgb(0,0,0); /* Fallback color */
+                background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+                color: white;
+                font-weight: bold;
+                border: 3px solid #f1f1f1;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                z-index: 2;
+                width: 80%;
+                padding: 20px;
+                text-align: center;
             }
         </style> 
 
@@ -86,7 +101,7 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
                 </li> -->
             </div>
             </nav>
-            <main role="main" class="container ">
+            <main role="main" class="container bg-text">
                 <div class="starter-template"> <br><br><br>
                     <h1>Analisis Foto</h1>
                     <span class="border-top my-3"></span>
